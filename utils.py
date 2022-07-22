ee760ccb0939788157e1c611e0ac23a3c260878e
Arno <arno-yu@outlook.com>
2022-07-22 11:05:12 +0800

    Add two dataclass DeviceCredential: to package username and password. NetworkDevice: to package the necessary info of connecting the device.

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from ipaddress import ip_address
-
+from dataclasses import dataclass
 
 def is_host_ip_address(host: str):
     try:
@@ -8,3 +8,14 @@ def is_host_ip_address(host: str):
             return True
     except ValueError:
         return False
+
+@dataclass
+class DeviceCredential:
+    username: str = ''
+    password: str = ''
+
+@dataclass
+class NetworkDevice:
+    host: str
+    domain_suffix: str = ''
+    credential: DeviceCredential = None
